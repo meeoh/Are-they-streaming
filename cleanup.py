49d3ce0fbd2c03341af1b694cc4fdf5ac172b7bb
Shameel Abdullah <shameel.abdullah@gmail.com>
2015-06-01 01:22:32 -0400

    Made script to filter streamers
    rough work

---
+++ cleanup.py
@@ -0,0 +1,19 @@
+import re
+
+writeFile = open('newstreamers.txt','w')
+with open('streamers.txt') as f:
+	for line in f:
+		foo = line.split(': ');
+		# link = re.search("(?P<url>https?://[^\s]+)", line).group("url")
+		# link = line.replace(' ', '')[:-5]
+		writeFile.write('{\'name\': \'' + foo[0] + '\', \'link\':\''  + foo[1].rstrip().split('[', 1)[0] + '\'},\n') ;
+	
+		
+
+		print foo
+
+
+
+
+
+#print re.search("(?P<url>https?://[^\s]+)", myString).group("url")
