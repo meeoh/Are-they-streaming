b75a7813ebc07f0b2b4ff01d5d80ca77b496fd67
Shameel Abdullah <shameel.abdullah@gmail.com>
2015-06-01 01:22:53 -0400

    Hardcoded all streamers in
    also basic html/css

---
+++ scripts/index.js
@@ -0,0 +1,163 @@
+var app = angular.module('isStreaming',[]);
+
+app.controller('usersController', function($scope,$http){
+
+
+	$scope.checkIfStreaming = function(link){
+
+		if($scope.link == link) return;
+		$scope.link = link;
+		
+		link = link.split('/').pop(-1)
+		console.log(link);
+		
+		$.ajax({
+    		url: "https://api.twitch.tv/kraken/streams/" + link,
+ 
+    		// The name of the callback parameter, as specified by the YQL service
+		    jsonp: "callback",
+ 
+    		// Tell jQuery we're expecting JSONP
+    		dataType: "jsonp",
+ 
+ 
+    // Work with the response
+    		success: function( response ) {
+        	console.log( response ); // server response
+        	return response.stream;
+    		}
+		});
+	};
+
+
+
+	$scope.users = [
+	{'name': 'abE (Affinity)', 'link':'http://www.twitch.tv/abecsgo'}
+	// {'name': 'adreN (Liquid)', 'link':'http://www.twitch.tv/adren_tv'},
+	// {'name': 'AdreN (HellRaisers)', 'link':'http://www.twitch.tv/adrentot'},
+	// {'name': 'Allu (NiP)', 'link':'http://www.twitch.tv/allub'},
+	// {'name': 'Anexis (Dignitas)', 'link':'http://www.twitch.tv/anexismsl'},
+	// {'name': 'Anger (LG)', 'link':'http://www.twitch.tv/angercss'},
+	// {'name': 'apEX (Titan)', 'link':'http://www.twitch.tv/apex'},
+	// {'name': 'Aurora (LGB Female)', 'link':'http://www.twitch.tv/auroracsgo'},
+	// {'name': 'autimatic (Nihilum)', 'link':'http://www.twitch.tv/autimatictv'},
+	// {'name': 'AZK', 'link':'http://www.twitch.tv/azkcs'},
+	// {'name': 'Benita (Ubinited)', 'link':'http://www.twitch.tv/benita29'},
+	// {'name': 'bMAX (Lady Lunatic)', 'link':'http://www.twitch.tv/bmax3'},
+	// {'name': 'Boltz (Keyd Stars)', 'link':'http://twitch.tv/ricardoboltz'},
+	// {'name': 'bondik (Flipsid3)', 'link':'http://www.twitch.tv/bondik_csgo'},
+	// {'name': 'byali (VP)', 'link':'http://www.twitch.tv/byalli'},
+	// {'name': 'cadiaN', 'link':'http://www.twitch.tv/cadianuz'},
+	// {'name': 'Cajunb (TSM)', 'link':'http://www.twitch.tv/cajunb'},
+	// {'name': 'Cath (Ubinited)', 'link':'http://www.twitch.tv/cath88'},
+	// {'name': 'ChrisJ (Mousesports)', 'link':'http://www.twitch.tv/chrisjcsgo/'},
+	// {'name': 'Cutler (CLG)', 'link':'http://www.twitch.tv/reltuc'},
+	// {'name': 'Davey (affNity)', 'link':'http://www.twitch.tv/daveycsgo'},
+	// {'name': 'DaZeD', 'link':'http://www.twitch.tv/godazed'},
+	// {'name': 'desi (eLevate)', 'link':'http://www.twitch.tv/desi_tv'},
+	// {'name': 'Device (TSM)', 'link':'http://www.twitch.tv/device'},
+	// {'name': 'devilwalk', 'link':'http://www.twitch.tv/devilwalk'},
+	// {'name': 'Dum0re (Mostly Harmless)', 'link':'http://www.twitch.tv/dum0re'},
+	// {'name': 'dupreeh (TSM)', 'link':'http://www.twitch.tv/dupreeh'},
+	// {'name': 'Edward (NaVi)', 'link':'http://www.twitch.tv/edward'},
+	// {'name': 'EliGE (Liquid)', 'link':'http://www.twitch.tv/eligez'},
+	// {'name': 'Emong', 'link':'http://www.twitch.tv/emongg'},
+	// {'name': 'Ex6TenZ (Titan)', 'link':'http://www.twitch.tv/ex6tenz'},
+	// {'name': 'FalleN (Keyd Stars)', 'link':'http://www.twitch.tv/gafallen'},
+	// {'name': 'Fer (Keyd Stars)', 'link':'http://www.twitch.tv/fpsfer'},
+	// {'name': 'Fl0m (LG)', 'link':'http://www.twitch.tv/fl0m'},
+	// {'name': 'flamie (NaVi)', 'link':'http://www.twitch.tv/flamieff'},
+	// {'name': 'flowsicK (Liquid)', 'link':'http://www.twitch.tv/flokithegreat'},
+	// {'name': 'Flusha (Fnatic)', 'link':'http://www.twitch.tv/flusha'},
+	// {'name': 'FNS (CLG)', 'link':'http://www.twitch.tv/gofns'},
+	// {'name': 'Fox (Kinguin)', 'link':'http://www.twitch.tv/officialfoxcn'},
+	// {'name': 'FugLy (Liquid)', 'link':'http://www.twitch.tv/fuglyy'},
+	// {'name': 'f0rest (NiP)', 'link':'http://www.twitch.tv/officialf0rest'},
+	// {'name': 'fREAKAZOID (C9)', 'link':'http://www.twitch.tv/freakazoid_tv'},
+	// {'name': 'Friberg (NiP)', 'link':'http://www.twitch.tv/officialfriberg'},
+	// {'name': 'friis (SK)', 'link':'http://www.twitch.tv/fnaticfriis'},
+	// {'name': 'Frozt (LG)', 'link':'http://www.twitch.tv/careyfrozt'},
+	// {'name': 'GeT_RiGhT (NiP)', 'link':'http://www.twitch.tv/officialgetright'},
+	// {'name': 'gobb (Mousesports)', 'link':'http://www.twitch.tv/gobbtv'},
+	// {'name': 'Goosebreeder (Lady Lunatic)', 'link':'http://www.twitch.tv/goosebreeder'},
+	// {'name': 'GuardiaN (NaVi)', 'link':'http://www.twitch.tv/rguardian'},
+	// {'name': 'hades (Tempo Storm)', 'link':'http://www.twitch.tv/hades_tv'},
+	// {'name': 'Happy (EnVyUs)', 'link':'http://www.twitch.tv/happycsgo'},
+	// {'name': 'Harvey (Ubinited)', 'link':'http://www.twitch.tv/missharvey'},
+	// {'name': 'Hazed (CLG)', 'link':'http://www.twitch.tv/hazedlol'},
+	// {'name': 'Hiko (Nihilum)', 'link':'http://www.twitch.tv/hiko'},
+	// {'name': 'HoochR', 'link':'http://www.twitch.tv/hoochrrr'},
+	// {'name': 'jdm64', 'link':'http://www.twitch.tv/jdm64'},
+	// {'name': 'jkaem (LGB)', 'link':'http://www.twitch.tv/jkaem'},
+	// {'name': 'jks (VOX)', 'link':'http://www.twitch.tv/jks_au'},
+	// {'name': 'Just9n (Method)', 'link':'http://www.twitch.tv/just9n'},
+	// {'name': 'JW (Fnatic)', 'link':'http://www.twitch.tv/jesperwow'},
+	// {'name': 'Karrigan (TSM)', 'link':'http://www.twitch.tv/karrigango'},
+	// {'name': 'kennyS (Titan)', 'link':'http://www.twitch.tv/kennys'},
+	// {'name': 'Kioshima (EnVyUs)', 'link':'http://www.twitch.tv/kiocsgo'},
+	// {'name': 'Kjaerbye (Dignitas)', 'link':'http://www.twitch.tv/kjaerbye98'},
+	// {'name': 'kHRYSTAL (Acer)', 'link':'http://www.twitch.tv/khrystal_cs'},
+	// {'name': 'kRYSTAL (PENTA)', 'link':'http://www.twitch.tv/krystal'},
+	// {'name': 'Ledyy (Karma)', 'link':'http://www.twitch.tv/ledymedinah'},
+	// {'name': 'LeX (LG)', 'link':'http://www.twitch.tv/lexcs'},
+	// {'name': 'Lynnski (Karma)', 'link':'http://www.twitch.tv/artStar_x'},
+	// {'name': 'Maikelele (Kinguin)', 'link':'http://www.twitch.tv/officialmaikelele'},
+	// {'name': 'Maniac (Titan)', 'link':'http://www.twitch.tv/Maniac'},
+	// {'name': 'MattieeW (LGB)', 'link':'http://www.twitch.tv/mattieew'},
+	// {'name': 'Melania (Epsilon)', 'link':'http://www.twitch.tv/officialmelania'},
+	// {'name': 'm0E', 'link':'http://www.twitch.tv/m0e_tv'},
+	// {'name': 'Natawhee', 'link':'http://www.twitch.tv/natawhee'},
+	// {'name': 'NBK (EnVyUs)', 'link':'http://www.twitch.tv/nbk'},
+	// {'name': 'Neo (VP)', 'link':'http://www.twitch.tv/neog5'},
+	// {'name': 'Nico (Dignitas)', 'link':'http://www.twitch.tv/nicobombs'},
+	// {'name': 'nitr0 (Liquid)', 'link':'http://www.twitch.tv/nitr0_tv'},
+	// {'name': 'n0thing (C9)', 'link':'http://www.twitch.tv/n0thingtv'},
+	// {'name': 'OCEAN (SKDC)', 'link':'http://www.twitch.tv/odeag'},
+	// {'name': 'Olofmeister (Fnatic)', 'link':'http://www.twitch.tv/olofmeister'},
+	// {'name': 'oskar (nEophyte)', 'link':'http://www.twitch.tv/oskartommy'},
+	// {'name': 'pashaBiceps (VP)', 'link':'http://www.twitch.tv/pashabiceps'},
+	// {'name': 'Pex (Ace)', 'link':'http://www.twitch.tv/officialpex'},
+	// {'name': 'Pimp (Dignitas)', 'link':'http://www.twitch.tv/pimpwinneche'},
+	// {'name': 'Polly (LGB)', 'link':'http://www.twitch.tv/polly717'},
+	// {'name': 'Potter (Ubinited)', 'link':'http://www.twitch.tv/omgitspotter'},
+	// {'name': 'pronax (Fnatic)', 'link':'http://www.twitch.tv/pronaxqwe'},
+	// {'name': 'Pterodactyls', 'link':'http://www.twitch.tv/pterodactylsftw'},
+	// {'name': 'PTR (LG)', 'link':'http://www.twitch.tv/ptr_tv'},
+	// {'name': 'Pyth (LG)', 'link':'http://www.twitch.tv/pythlfc'},
+	// {'name': 'rain (Kinguin)', 'link':'http://www.twitch.tv/raingomg'},
+	// {'name': 'Rain (Karma)', 'link':'http://www.twitch.tv/rain_4'},
+	// {'name': 'rebelzwow', 'link':'http://www.twitch.tv/rebelzwow'},
+	// {'name': 'Relyks', 'link':'http://www.twitch.tv/relyksog'},
+	// {'name': 'Rickeh (Immunity)', 'link':'http://www.twitch.tv/rickehaus'},
+	// {'name': 'Robwiz (Noble)', 'link':'http://www.twitch.tv/robwiztv'},
+	// {'name': 'RUBINO (LGB)', 'link':'http://www.twitch.tv/rubinoeu'},
+	// {'name': 'ryx (Tempo Storm)', 'link':'http://www.twitch.tv/kingronnie'},
+	// {'name': 'Schneider', 'link':'http://www.twitch.tv/officialschneider'},
+	// {'name': 'Scream (Kinguin)', 'link':'http://www.twitch.tv/scream'},
+	// {'name': 'Seangares (C9)', 'link':'http://www.twitch.tv/sgares'},
+	// {'name': 'Seized (NaVi)', 'link':'http://www.twitch.tv/seizedwf'},
+	// {'name': 'Semphis (Nihilum)', 'link':'http://www.twitch.tv/semphis'},
+	// {'name': 'Shahzam (Tempo Storm)', 'link':'http://www.twitch.tv/shahzam'},
+	// {'name': 'Shox (EnVyUs)', 'link':'http://www.twitch.tv/shoxiejesuss'},
+	// {'name': 'Shroud (C9)', 'link':'http://www.twitch.tv/meclipse'},
+	// {'name': 'Silent3m (Method)', 'link':'http://www.twitch.tv/silent3m'},
+	// {'name': 'Skadoodle (C9)', 'link':'http://www.twitch.tv/skadoodle'},
+	// {'name': 'Smithzz (EnVyUs)', 'link':'http://www.twitch.tv/smithzz'},
+	// {'name': 'Snax (VP)', 'link':'http://www.twitch.tv/snaxik'},
+	// {'name': 'SpawN', 'link':'http://www.twitch.tv/spawn4real'},
+	// {'name': 'SPUNJ (VOX)', 'link':'http://www.twitch.tv/voxespunj'},
+	// {'name': 'Steel', 'link':'http://www.twitch.tv/steel_tv'},
+	// {'name': 'streb0r (Method)', 'link':'http://www.twitch.tv/streb0r'},
+	// {'name': 'strux1 (PENTA)', 'link':'http://www.twitch.tv/strux1'},
+	// {'name': 'stunna (C9)', 'link':'http://www.twitch.tv/stunna_'},
+	// {'name': 'Swag (C9)', 'link':'http://www.twitch.tv/swagcs'},
+	// {'name': 'Tarik (CLG)', 'link':'http://www.twitch.tv/tarik_tv'},
+	// {'name': 'TaZ (VP)', 'link':'http://www.twitch.tv/g5taz'},
+	// {'name': 'WorldEdit (Flipsid3)', 'link':'http://www.twitch.tv/worldedit'},
+	// {'name': 'Xizt (NiP)', 'link':'http://www.twitch.tv/officialxizt'},
+	// {'name': 'Yaman (VOX)', 'link':'http://www.twitch.tv/yamanlol'},
+	// {'name': 'Zeus (NaVi)', 'link':'http://www.twitch.tv/zeus'}
+	];
+
+
+});
